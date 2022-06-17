c195bb22698a31c46ef78388935689cff5618247
Matteo Monti <matteo.monti@epfl.ch>
2022-06-17 17:44:20 +0200

    Scaffold `rendezvous`

--- src/rendezvous.rs
+++ src/rendezvous.rs
@@ -1,1 +1,21 @@
-pub(crate) async fn rendezvous(_servers: usize, _brokers: usize) {}
+use std::time::Duration;
+
+use talk::link::rendezvous::{
+    Server as RendezvousServer, ServerSettings as RendezvousServerSettings,
+};
+
+use tokio::time;
+
+const ADDRESS: &str = "0.0.0.0:6000";
+
+pub(crate) async fn rendezvous(servers: usize, brokers: usize) {
+    let settings = RendezvousServerSettings {
+        shard_sizes: vec![servers, brokers],
+    };
+
+    let _rendezvous = RendezvousServer::new(ADDRESS, settings).await.unwrap();
+
+    loop {
+        time::sleep(Duration::from_secs(1)).await;
+    }
+}
