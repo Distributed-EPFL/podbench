0a07f1a06e27b5707752644697622e37a52d0b84
Matteo Monti <matteo.monti@epfl.ch>
2022-06-17 17:59:37 +0200

    Add logging to `rendezvous`

--- src/rendezvous.rs
+++ src/rendezvous.rs
@@ -9,11 +9,18 @@ use tokio::time;
 const ADDRESS: &str = "0.0.0.0:6000";
 
 pub(crate) async fn rendezvous(servers: usize, brokers: usize) {
+    println!("Booting rendezvous server..");
+    println!("  Expected servers: {}", servers);
+    println!("  Expected brokers: {}", brokers);
+    println!();
+
     let settings = RendezvousServerSettings {
         shard_sizes: vec![servers, brokers],
     };
 
     let _rendezvous = RendezvousServer::new(ADDRESS, settings).await.unwrap();
+
+    println!("Rendezvous server running.");
 
     loop {
         time::sleep(Duration::from_secs(1)).await;
